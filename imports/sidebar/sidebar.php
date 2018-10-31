f5540c9a30d82e68b75a351f3a16a11c2c3034cb
Anderson <anderson@zumerata.com.br>
2018-10-31 13:18:48 -0300

    Comecei a configurar o sidebar

---
+++ imports/sidebar/sidebar.php
@@ -0,0 +1,62 @@
+<link rel="stylesheet" type="text/css" href="../../imports/sidebar/sidebar.css">
+
+<div class="nav-side-menu">
+    <div class="brand">Pizzaria</div>
+    <i class="fa fa-bars fa-2x toggle-btn" data-toggle="collapse" data-target="#menu-content"></i>
+
+    <div class="menu-list">
+
+        <ul id="menu-content" class="menu-content collapse out">
+            <li>
+                <a href="#">
+                    <i class="fas fa-home fa-lg"></i> Página Inicial
+                </a>
+            </li>
+
+            <li data-toggle="collapse" data-target="#products" class="collapsed active">
+                <a href="#"><i class="fas fa-utensils fa-lg"></i> Produção <span class="arrow"></span></a>
+            </li>
+            <ul class="sub-menu collapse" id="products">
+                <li class="active"><a href="#">CSS3 Animation</a></li>
+                <li><a href="#">General</a></li>
+                <li><a href="#">Buttons</a></li>
+                <li><a href="#">Tabs & Accordions</a></li>
+                <li><a href="#">Typography</a></li>
+                <li><a href="#">FontAwesome</a></li>
+                <li><a href="#">Slider</a></li>
+                <li><a href="#">Panels</a></li>
+                <li><a href="#">Widgets</a></li>
+                <li><a href="#">Bootstrap Model</a></li>
+            </ul>
+
+
+            <li data-toggle="collapse" data-target="#service" class="collapsed">
+                <a href="#"><i class="fas fa-book"></i> Salão <span class="arrow"></span></a>
+            </li>
+            <ul class="sub-menu collapse" id="service">
+                <li>New Service 1</li>
+                <li>New Service 2</li>
+                <li>New Service 3</li>
+            </ul>
+
+
+            <li data-toggle="collapse" data-target="#new" class="collapsed">
+                <a href="#"><i class="far fa-money-bill-alt fa-lg"></i> Caixa <span class="arrow"></span></a>
+            </li>
+            <ul class="sub-menu collapse" id="new">
+                <li>New New 1</li>
+                <li>New New 2</li>
+                <li>New New 3</li>
+            </ul>
+
+            <li data-toggle="collapse" data-target="#settings" class="collapsed">
+                <a href="#"><i class="fas fa-wrench fa-lg"></i> Configurações <span class="arrow"></span></a>
+            </li>
+            <ul class="sub-menu collapse" id="settings">
+                <li>New New 1</li>
+                <li>New New 2</li>
+                <li><a href="../login/login.php">Sair</a></li>
+            </ul>
+        </ul>
+    </div>
+</div>
