58b3b45b7a5372c97b9cff586e308f682efe1884
Anderson <anderson@zumerata.com.br>
2018-11-14 20:12:56 -0200

    inicio do desenvolvimento do crud das comandas

---
+++ pages/commands/add/add-command.js
@@ -0,0 +1,11 @@
+qtd = 0;
+
+function sum(){
+    qtd ++;
+    console.log(qtd);
+}
+
+function sub() {
+    if(qtd > 0) qtd--;
+    console.log(qtd);
+}
--- pages/commands/add/add-command.php
+++ pages/commands/add/add-command.php
@@ -6,19 +6,41 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
         <!-- Stylesheets -->
-        <link rel="stylesheet" type="text/css" href="../../frameworks/bootstrap/css/bootstrap.min.css">
-        <link rel="stylesheet" type="text/css" href="../../frameworks/bootstrap/css/bootstrap-grid.min.css">
-        <link rel="stylesheet" type="text/css" media="screen" href="../../assets/css/global.css" />
+        <link rel="stylesheet" type="text/css" href="../../../frameworks/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="../../../frameworks/bootstrap/css/bootstrap-grid.min.css">
+        <link rel="stylesheet" type="text/css" media="screen" href="../../../assets/css/global.css" />
         <link rel="stylesheet" type="text/css" media="screen" href="add-command.css" />
         
         <!-- Script -->
-        <script src="../../frameworks/jQuery/jquery-3.3.1.min.js"></script>
-        <script src="../../frameworks/bootstrap/js/bootstrap.min.js"></script>
-        <script src="../../assets/js/global.js"></script>
+        <script src="../../../frameworks/jQuery/jquery-3.3.1.min.js"></script>
+        <script src="../../../frameworks/bootstrap/js/bootstrap.min.js"></script>
+        <script src="../../../assets/js/global.js"></script>
+        <script src="add-command.js"></script>
 </head>
 
 
 <body>
-
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-12 col-md-4"></div>
+            <div class="col-sm-12 col-md-4">
+                <div class="card">
+                    <div class="card-header">Novo Pedido</div>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col">
+                                <button onClick="sub()">-</button>
+                            </div>
+                            <div class="col">Inteiro</div>
+                            <div class="col">
+                                <button onClick="sum()">+</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-4"></div>
+        </div>
+    </div>
 </body>
 </html>
